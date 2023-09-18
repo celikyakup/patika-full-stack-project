c55810d512531da81d291c03e18579b714b9a933
Yakup Çelik <yakupcelik1.a@gmail.com>
2023-09-18 17:32:00 +0300

    asal sayi

--- week-2/recursive-asal-sayi/src/Main.java
+++ week-2/recursive-asal-sayi/src/Main.java
@@ -2,21 +2,22 @@ import java.util.Scanner;
 public class Main {
     static boolean isPrime(int num,int i){
         // Base cases
-        if (num <= 2)
-            return (num == 2) ? true : false;
-        if (num % i == 0)
+        if (num < 2) {
             return false;
-        if (i * i > num)
+        } else if ( i== 1) {
             return true;
-        // Check for next divisor
-        return isPrime(num, i + 1);
+        } else if (num % i == 0) {
+            return false;
+        } else {
+            return isPrime(num, i - 1);
+        }
     }
 
     public static void main(String[] args) {
         Scanner input= new Scanner(System.in);
         System.out.print("Değeri giriniz:");
         int num= input.nextInt();
-        if(isPrime(num,2)) {
+        if(isPrime(num,num-1)) {
             System.out.println(num+" sayısı ASALDIR.");
         }
         else {
