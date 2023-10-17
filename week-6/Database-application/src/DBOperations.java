f7c59e4d72a1731d3956d1658b2314551e65b606
Yakup Çelik <yakupcelik1.a@gmail.com>
2023-10-17 15:06:03 +0300

    Database Applications

---
+++ week-6/Database-application/src/DBOperations.java
@@ -0,0 +1,25 @@
+import java.sql.*;
+
+public class DBOperations {
+    public static final String DB_URL="jdbc:mysql://localhost/university";
+    public static final String DB_USER="root";
+    public static final String DB_PASSWORD="mysql";
+    public static void main(String[] args) {
+        Connection connect=null;
+        String sql="SELECT * FROM employees";
+        try {
+            connect= DriverManager.getConnection(DB_URL,DB_USER,DB_PASSWORD);
+            Statement st= connect.createStatement();
+            ResultSet data=st.executeQuery(sql);
+            while (data.next()){
+                System.out.print("ID: "+data.getInt("employe_id"));
+                System.out.print("\tName: "+data.getString("employe_name"));
+                System.out.print("\tPosition: "+data.getString("employe_position"));
+                System.out.print("\tSalary: "+data.getInt("employe_salary"));
+                System.out.println();
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());;
+        }
+    }
+}
