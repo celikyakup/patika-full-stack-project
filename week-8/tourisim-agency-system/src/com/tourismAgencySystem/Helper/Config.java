17352e3b5ebb75b4589d8d10fb79b03a95a9e7f9
Yakup Çelik <yakupcelik1.a@gmail.com>
2023-11-07 14:22:55 +0300

    update repo

--- week-8/tourisim-agency-system/src/com/tourismAgencySystem/Helper/Config.java
+++
@@ -1,8 +0,0 @@
-package com.tourismAgencySystem.Helper;
-
-public class Config {
-    public static final String PROJECT_TITTLE="Tourism Agency System";
-    public static final String DB_URL="jdbc:mysql://localhost/tourism_agency";
-    public static final String DB_USERNAME="root";
-    public static final String DB_PASSWORD="mysql";
-}
