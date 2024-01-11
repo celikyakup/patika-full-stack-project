94099682290ad73e50943872de6ebc1ba6cebb4f
Yakup Çelik <yakupcelik1.a@gmail.com>
2024-01-11 10:50:26 +0300

    Update clock.js

--- week-19/javascriptclock/clock.js
+++ week-19/javascriptclock/clock.js
@@ -1,41 +1,29 @@
-let myName=document.querySelector("strong>#myName");
-
 let userName=prompt("Lütfen İsminizi giriniz ")
-
+let myName=document.querySelector("strong>#myName");
 myName.append(`${userName[0].toUpperCase()+userName.substring(1)}`)
+
+function addZeroLessThanTen(dateValue){
+    let dateSyntax="";
+    if(dateValue<10){
+        dateSyntax=`0${dateValue}`;
+    }
+    else{
+        dateSyntax=dateValue;
+    }
+    return dateSyntax;
+}
 
 function clock(){
     let date=new Date()
-    let hours=date.getHours()
-    let minutes=date.getMinutes()
-    let second=date.getUTCSeconds()
+    let hours=addZeroLessThanTen(date.getHours())
+    let minutes=addZeroLessThanTen(date.getMinutes())
+    let second=addZeroLessThanTen(date.getUTCSeconds())
     let days=["Pazar","Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi"]
     let day=days[date.getDay()]
     let activeHours=document.querySelector("#myClock")
-    if(hours<10){
-        activeHours.innerHTML=(`0${hours}:${minutes}:${second} ${day}`)
-    }
-    else if(minutes<10){
-        activeHours.innerHTML=(`${hours}:0${minutes}:${second} ${day}`)
-    }
-    else if(second<10){
-        activeHours.innerHTML=(`${hours}:${minutes}:0${second} ${day}`)
-    }
-    else if(hours<10 && minutes<10){
-        activeHours.innerHTML=(`0${hours}:0${minutes}:${second} ${day}`)
-    }
-    else if(minutes<10 && second<10){
-        activeHours.innerHTML=(`${hours}:0${minutes}:0${second} ${day}`)
-    }
-    else if(hours<10 && second<10){
-        activeHours.innerHTML=(`0${hours}:${minutes}:0${second} ${day}`)
-    }
-    else if(hours<10 && minutes<10 && second<10){
-        activeHours.innerHTML=(`0${hours}:0${minutes}:0${second} ${day}`)
-    }
-    else{
-        activeHours.innerHTML=(`${hours}:${minutes}:${second} ${day}`)
-    }
+    
+    activeHours.innerHTML=(`${hours}:${minutes}:${second} ${day}`)
+    
 }
 
 setInterval(clock,1000)
