07aa6bd75ed83ab711c68d1b3d60b8cfdde21714
Yakup Çelik <yakupcelik1.a@gmail.com>
2024-01-17 17:01:10 +0300

    update to do list

---
+++ week-20/todolist/README.md
@@ -0,0 +1,7 @@
+To Do List Web Page
+
+[Project Link](https://celikyakup.github.io/patika-full-stack-project/week-20/todolist/)
+
+Project View
+
+![Alt text](image.png)
--- week-20/todolist/javascript.js
+++ week-20/todolist/javascript.js
@@ -1,11 +1,13 @@
-let toDoList=localStorage.getItem("to-do-list") ? JSON.parse(localStorage.getItem("to-do-list")) : [] ;
+let toDoList=localStorage.getItem("to-do-list") ? JSON.parse(localStorage.getItem("to-do-list")) : new Map() ;
 
 let listDom=document.querySelector("#list")
-toDoList.forEach(todo=>{
+
+for(let todo in toDoList){
     let liDom=document.createElement("li");
     liDom.innerHTML=todo; 
     listDom.appendChild(liDom);
-})
+}
+
 
 let myNodelist = document.getElementsByTagName("LI");
     for (let i = 0; i < myNodelist.length; i++) {
@@ -22,24 +24,41 @@ close.forEach(event=>{
     event.onclick=function(){
         let li=this.parentElement;
         li.remove();
-        toDoList.splice(li,1);
-        console.log(toDoList);
-        localStorage.setItem("to-do-list",JSON.stringify(toDoList))
+        //console.log(li.textContent.split("×")[0]);
+        delete toDoList[li.textContent.split("×")[0]];
+        localStorage.setItem("to-do-list",JSON.stringify(toDoList));
     }
 })
 
+for(let check in toDoList){
+    if (toDoList.hasOwnProperty(check)){
+        let status=toDoList[check].status;
+        console.log(status)
+        if(status){
+            let listItem=$("li:contains('"+check+"')");
+            if(listItem){
+                listItem.addClass("checked");
+            }
+        }
+    }
+}
 
 listDom.addEventListener("click",function(e){
-    if(e.target.tagName==="LI"){
-        e.target.classList.toggle("checked");
+    
+    if(e.target.tagName==="LI" ){
+
+        //console.log(e.target.textContent.split("×")[0])
+        toDoList[e.target.textContent.split("×")[0]].status=!toDoList[e.target.textContent.split("×")[0]].status;
+        localStorage.setItem("to-do-list",JSON.stringify(toDoList));
+        e.target.classList.toggle("checked",toDoList[e.target.textContent.split("×")[0]].status)
+        
     }
 })
-
 
 function newElement(){
     
     let taskDom=document.querySelector("#task");
-    let listDom=document.querySelector("#list");
+    //let listDom=document.querySelector("#list");
     let liDom=document.createElement("li");
     
     if(taskDom.value.trim()===""){
@@ -51,7 +70,10 @@ function newElement(){
     else {
         listDom.appendChild(liDom);
         liDom.innerHTML=taskDom.value;
-        toDoList.push(taskDom.value);
+        toDoList[taskDom.value]={
+            "do":taskDom.value,
+            "status":false
+        }
         $("#liveToastSuccess").toast("show");
         localStorage.setItem("to-do-list",JSON.stringify(toDoList))
         taskDom.value="";
@@ -71,10 +93,10 @@ function newElement(){
         event.onclick=function(){
             let li=this.parentElement;
             li.remove();
-            toDoList.splice(li,1);
-            console.log(toDoList);
-            localStorage.setItem("to-do-list",JSON.stringify(toDoList))
-        }
+            //console.log(li.textContent.split("×")[0]);
+            delete toDoList[li.textContent.split("×")[0]];
+            localStorage.setItem("to-do-list",JSON.stringify(toDoList));
+            }
     })
 
 }
